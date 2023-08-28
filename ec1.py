77047df0386cb6e7d08f63c1fdaa5851fdfd8e31
sidrolffag <143381895+sidrolffag@users.noreply.github.com>
2023-08-28 14:10:39 +0700

    homework duep

--- ec1.py
+++ ec1.py
@@ -1,43 +1,41 @@
-from random import randint
-
-def obscure_function_1(a, b):
-    c = 0
+def obscure_function_with_a_descriptive_name_that_does_something_complicated(a_parameter_with_a_long_name, another_parameter_with_a_long_name):
+    a_meaningful_variable_name = 0
     for _ in range(100):
-        c += a * b - (a + b) / (a - b)
-    return c
+        a_meaningful_variable_name += a_parameter_with_a_long_name * another_parameter_with_a_long_name - (a_parameter_with_a_long_name + another_parameter_with_a_long_name) / (a_parameter_with_a_long_name - another_parameter_with_a_long_name)
+    return a_meaningful_variable_name
 
-def obscure_function_2(n):
+def a_complicated_recursive_function_with_a_long_name(n):
     if n == 0:
         return 1
     else:
-        return n * obscure_function_2(n - 1) + (obscure_function_2(n - 1) - obscure_function_2(n - 2))
+        return n * a_complicated_recursive_function_with_a_long_name(n - 1) + (a_complicated_recursive_function_with_a_long_name(n - 1) - a_complicated_recursive_function_with_a_long_name(n - 2))
 
-def obscure_function_3(lst):
-    lst_len = len(lst)
-    if lst_len <= 1:
+def a_descriptive_function_name_for_sorting_a_list(lst):
+    list_length = len(lst)
+    if list_length <= 1:
         return lst
     else:
-        pivot = lst[lst_len // 2]
-        smaller = obscure_function_3([x for x in lst if x < pivot])
-        larger = obscure_function_3([x for x in lst if x > pivot])
+        pivot = lst[list_length // 2]
+        smaller = a_descriptive_function_name_for_sorting_a_list([x for x in lst if x < pivot])
+        larger = a_descriptive_function_name_for_sorting_a_list([x for x in lst if x > pivot])
         return smaller + [x for x in lst if x == pivot] + larger
 
-def create_obscure_data_structure():
+def create_a_complex_data_structure_with_a_descriptive_name():
     data = []
     for _ in range(10):
         sub_data = []
-        for _ in range(randint(1, 5)):
-            sub_data.append(obscure_function_1(_, _ + 1))
+        for sub_index in range(randint(1, 5)):
+            sub_data.append(obscure_function_with_a_descriptive_name(_, sub_index + 1))
         data.append(sub_data)
     return data
 
 if __name__ == "__main__":
-    result_1 = obscure_function_1(5, 7)
-    result_2 = obscure_function_2(4)
-    result_3 = obscure_function_3([9, 2, 5, 1, 7, 3, 8, 4, 6])
-    data_structure = create_obscure_data_structure()
+    result_of_obscure_function = obscure_function_with_a_descriptive_name(5, 7)
+    result_of_recursive_function = a_complicated_recursive_function_with_a_long_name(4)
+    result_of_sorting_function = a_descriptive_function_name_for_sorting_a_list([9, 2, 5, 1, 7, 3, 8, 4, 6])
+    complex_data_structure = create_a_complex_data_structure_with_a_descriptive_name()
     
-    print("Result 1:", result_1)
-    print("Result 2:", result_2)
-    print("Result 3:", result_3)
-    print("Data Structure:", data_structure)
+    print("Result of Obscure Function:", result_of_obscure_function)
+    print("Result of Recursive Function:", result_of_recursive_function)
+    print("Result of Sorting Function:", result_of_sorting_function)
+    print("Complex Data Structure:", complex_data_structure)
